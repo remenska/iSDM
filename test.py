a5ab7b5ec64380dcaf859d62178be798de3e6279
daniela <remenska@gmail.com>
2016-04-18 10:54:08 +0200

    convert tabs to spaces -.-

--- test.py
+++ test.py
@@ -5,12 +5,12 @@ import sys
 command = 'Rscript'
 
 if __name__ == '__main__':
-	path2script = "./simple.R"
+    path2script = "./simple.R"
 
-	cmd = [command, path2script] + sys.argv[1:]
-	print("cmd is %s" %cmd)
+    cmd = [command, path2script] + sys.argv[1:]
+    print("cmd is %s" %cmd)
 
-	x = subprocess.check_output(cmd, universal_newlines=True)
-	print("cmd output is: ")
-	print("%s" %str(x))
+    x = subprocess.check_output(cmd, universal_newlines=True)
+    print("cmd output is: ")
+    print("%s" %str(x))
 
