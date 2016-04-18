e97da76b77626ce98ce6b62c1a8a2242044da8c9
daniela <remenska@gmail.com>
2016-04-18 13:30:37 +0200

    rm.

---
+++ iSDM/environment.py
@@ -0,0 +1,22 @@
+
+class EnvironmentalMap(object):
+    def __init__(self, **kwargs):
+        self.resolution = kwargs['resolution']
+
+    def convert_to(resolution):
+        pass
+
+    def load_data(self, file_path=None):
+        pass
+        
+
+class ClimateMap(EnvironmentalMap):
+    pass
+
+class LandCoverMap(EnvironmentalMap):
+    pass
+
+class LandUse(EnvironmentalMap):
+    pass
+
+
