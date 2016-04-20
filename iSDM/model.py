ec158e3df99ab0cb5c7e6bdd83c848e9b506e6cd
daniela <remenska@gmail.com>
2016-04-20 11:51:50 +0200

    space tab

--- iSDM/model.py
+++ iSDM/model.py
@@ -7,34 +7,34 @@ logger.setLevel(logging.DEBUG)
 
 
 class Algorithm(Enum):
-	GAM = 1
-	GLM = 2
-	MAXENT = 3
+    GAM = 1
+    GLM = 2
+    MAXENT = 3
 
 
 class Evaluation(Enum):
-	ROC = 1
-	KAPPA = 2
-	TSS = 3
+    ROC = 1
+    KAPPA = 2
+    TSS = 3
 
 
 class Model(object):
 
-	algorithm = None
-	method = None
+    algorithm = None
+    method = None
   species = None
 
-	def __init__(self, algorithm = Algorithm.GAM):
-		self.algorithm = algorithm
+    def __init__(self, algorithm = Algorithm.GAM):
+        self.algorithm = algorithm
 
-	def cross_validate(self, percentage, random_seed): 
-		pass
+    def cross_validate(self, percentage, random_seed): 
+        pass
 
-	def evaluate_performance(self, method = Evaluation.ROC, **kwargs):
-		self.method = method
+    def evaluate_performance(self, method = Evaluation.ROC, **kwargs):
+        self.method = method
 
-	def fit(self):
-		pass
+    def fit(self):
+        pass
 
 
 
