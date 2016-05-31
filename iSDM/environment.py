69e615e06d74ae54cf60021df9b136a1ee5d0d0c
daniela <remenska@gmail.com>
2016-05-31 12:03:56 +0200

    overlay climate raster data with polygon shapes

--- iSDM/environment.py
+++ iSDM/environment.py
@@ -6,6 +6,10 @@ import pprint
 from rasterio.warp import calculate_default_transform, RESAMPLING
 from geopandas import GeoSeries
 from iSDM.species import IUCNSpecies
+from affine import Affine
+import rasterio.features
+import numpy as np
+
 
 logger = logging.getLogger('iSDM.environment')
 logger.setLevel(logging.DEBUG)
@@ -39,19 +43,6 @@ class EnvironmentalLayer(object):
     def get_source(self):
         return self.source.name
 
-    def overlay(self, range_map):
-        """
-        To extract data from a raster only where it intersects with a vector feature.
-        rasterio provides option to "burn" vector shapes into rasters (rasterize the geometry). Then we create
-        a raster mask layer
-        """
-        if not (isinstance(range_map, GeoSeries)
-                or isinstance(range_map, EnvironmentalLayer)
-                or isinstance(range_map, IUCNSpecies)):
-            raise AttributeError("Please provide a correct rangemap input.")
-
-        logger.info("Overlayed raster climate data with the given range map.")
-
 
 class ClimateLayer(EnvironmentalLayer):
     def __init__(self, source=None, file_path=None, **kwargs):
@@ -65,7 +56,7 @@ class ClimateLayer(EnvironmentalLayer):
             raise AttributeError("Please provide a file_path argument to load the data from.")
 
         logger.info("Loading data from %s " % self.file_path)
-        climate_data = rasterio.open(self.file_path)
+        climate_data = rasterio.open(self.file_path, 'r')
         self.metadata = climate_data.meta
         self.resolution = climate_data.res
         self.bounds = climate_data.bounds
@@ -128,6 +119,53 @@ class ClimateLayer(EnvironmentalLayer):
                                             resampling=resampling
                                             )
             logger.info("Reprojected data in %s " % destination_file)
+
+    def overlay(self, range_map, plot=False):
+        """
+        To extract data from a raster only where it intersects with a vector feature.
+        rasterio provides option to "burn" vector shapes into rasters (rasterize the geometry). Then we create
+        a raster mask layer
+        """
+        if not (isinstance(range_map, GeoSeries)
+                or isinstance(range_map, EnvironmentalLayer)
+                or isinstance(range_map, IUCNSpecies)):
+            raise AttributeError("Please provide a correct rangemap input.")
+
+        # TODO: what about if the overlay is just a shape file?
+        # TODO: what if there are multiple geometries in a shapefile? Currently it just returns the last
+        # but could make a list and append
+        masked_data = None
+
+        if isinstance(range_map, EnvironmentalLayer) or isinstance(range_map, IUCNSpecies):
+            for geometry in range_map.get_data()['geometry']:
+                if self.climate_data.closed:
+                    self.load_dataset(self.file_path)
+                with self.climate_data as raster:
+                    # get pixel coordinates of the geometry's bounding box
+                    ul = raster.index(*geometry.bounds[0:2])
+                    lr = raster.index(*geometry.bounds[2:4])
+
+                    # read the subset of the data into a numpy array
+                    window = ((lr[0], ul[0] + 1), (ul[1], lr[1] + 1))
+                    data = raster.read(1, window=window)
+                    # create an affine transform for the subset data
+                    t = raster.affine
+                    shifted_affine = Affine(t.a, t.b, t.c + ul[1] * t.a, t.d, t.e, t.f + lr[0] * t.e)
+                    # rasterize the geometry
+                    mask = rasterio.features.rasterize(
+                        [(geometry, 0)],
+                        out_shape=data.shape,
+                        transform=shifted_affine,
+                        fill=1,
+                        all_touched=True,
+                        dtype=np.uint8)
+
+                    # create a masked numpy array
+                    masked_data = np.ma.array(data=data, mask=mask.astype(bool))
+
+        self.masked_data = masked_data
+        logger.info("Overlayed raster climate data with the given range map.")
+        logger.info("Use the .masked_data attribute to access it.")
 
 
 class LandCoverlayer(EnvironmentalLayer):   # this would e a shapefile, not a raster?
