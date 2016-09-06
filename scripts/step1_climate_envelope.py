c351b9bd2c9c82d045dae0891c79f1199b940945
daniela <remenska@gmail.com>
2016-09-06 15:56:01 +0200

    with cwd

--- scripts/step1_climate_envelope.py
+++ scripts/step1_climate_envelope.py
@@ -43,10 +43,10 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-r', '--realms-location', default=os.path.join("data", "terrestrial_ecoregions"), help='The full path to the folder where the biogeographic realms shapefiles are located.')
-parser.add_argument('-t', '--temperature-location', default=os.path.join("data", "watertemp"), help="The folder where the temperature raster files are.")
-parser.add_argument('-s', '--species-location', default=os.path.join("data", "fish"), help="The folder where the IUCN species shapefiles are located.")
-parser.add_argument('-o', '--output-location', default=os.path.join("data", "fish"), help="Output location (folder) for storing the output of the processing.")
+parser.add_argument('-r', '--realms-location', default=os.path.join(os.getcwd(), "data", "terrestrial_ecoregions"), help='The full path to the folder where the biogeographic realms shapefiles are located.')
+parser.add_argument('-t', '--temperature-location', default=os.path.join(os.getcwd(), "data", "watertemp"), help="The folder where the temperature raster files are.")
+parser.add_argument('-s', '--species-location', default=os.path.join(os.getcwd(), "data", "fish"), help="The folder where the IUCN species shapefiles are located.")
+parser.add_argument('-o', '--output-location', default=os.path.join(os.getcwd(), "data", "fish"), help="Output location (folder) for storing the output of the processing.")
 parser.add_argument('--reprocess', action='store_true', help="Reprocess the data, using the already-rasterized individual species rangemaps. Assumes these files are all available.")
 parser.set_defaults(reprocess=False)
 args = parser.parse_args()
